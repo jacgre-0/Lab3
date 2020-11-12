20428e917992077129b924364cdbc056e5cdb19b
_Jacce_ <jacob.ogren2@gmail.com>
2020-11-12 07:49:29 +0100

    Added function to calculate the average salary of employees

--- Average salaries/main.cpp
+++ Average salaries/main.cpp
@@ -1,3 +1,32 @@
+#include <iostream>
+
+float average_salary(const int* salaries, int length);
+
 int main() {
+    int size{};
+    int salary{};
+    int* salaryArray = nullptr;
+
+    std::cout << "Give total number of employees: ";
+    std::cin >> size;
+
+    salaryArray = new int[size];
+    for (int i = 0; i < size; i++) {
+        std::cout << "Give salary: ";
+        std::cin >> salaryArray[i];
+    }
+
+    std::cout << average_salary(salaryArray, size);
+
+    delete[] salaryArray;
     return 0;
+}
+
+float average_salary(const int* salaries, int length) {
+    int total{};
+
+    for (int i = 0; i < length; i++) {
+        total += *salaries++;
+    }
+    return static_cast<float>(total) / length;
 }
