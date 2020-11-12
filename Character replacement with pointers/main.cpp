3811d9049e07e267725fe85ba447921171aaa778
_Jacce_ <jacob.ogren2@gmail.com>
2020-11-12 09:20:38 +0100

    Added replace_chars function with example

--- Character replacement with pointers/main.cpp
+++ Character replacement with pointers/main.cpp
@@ -1,3 +1,20 @@
+#include <iostream>
+
+void replace_chars(char* str, char searhChar, char replacementChar);
+
 int main() {
+    char str[] = "Hello";
+    replace_chars(str, 'l', 'p');
+    replace_chars(str, 'H', 'l');
+    std::cout << str << std::endl;
     return 0;
+}
+
+void replace_chars(char* str, char searhChar, char replacementChar) {
+    while (*str != '\0') {
+        if (*str == searhChar) {
+            *str = replacementChar;
+        }
+        str++;
+    }
 }
