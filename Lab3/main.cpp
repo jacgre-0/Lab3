f11a469a7f2a7fe56169899ca5e0b5f8aa650e1c
_Jacce_ <jacob.ogren2@gmail.com>
2020-11-12 07:30:06 +0100

    Added functions that returns the length of a string (char array ptr)

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,3 +1,18 @@
-int main() {
+#include <iostream>
 
+int findLenghtOfString(char* str);
+
+int main() {
+    char arr[] = "";
+    int length = findLenghtOfString(arr);
+
+    std::cout << length << std::endl;
+}
+
+int findLenghtOfString(char* str) {
+    for (int i = 0; ; i++) {
+        if (*str++ == '\0') {
+            return i;
+        }
+    }
 }
